f37da681df4ea6dfc6df04ba8aaa746e098d6e58
faisalmobile512 <faisalf1677@gmail.com>
2026-09-15 12:39:37 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
-// Paste your Supabase project values here.
-// Do NOT put a service_role key here. Use the public anon key only.
 window.FAISAL_CONFIG = {
-  SUPABASE_URL: "PASTE_SUPABASE_PROJECT_URL_HERE",
-  SUPABASE_ANON_KEY: "PASTE_SUPABASE_ANON_KEY_HERE",
+  SUPABASE_URL: "https://hsgzsdouooomjlwlimod.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_1nVrYHycV3UpMI5dkaKjsw_wZQGFaVv",
   WHATSAPP_NUMBER: "923105120312"
 };
